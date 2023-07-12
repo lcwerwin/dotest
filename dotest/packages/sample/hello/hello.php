c6912f4a95b20a3221fe3e843b23771495d56193
Erwin <lcwerwin@gmail.com>
2023-07-12 14:11:51 +0800

    Update hello.php

--- dotest/packages/sample/hello/hello.php
+++ dotest/packages/sample/hello/hello.php
@@ -6,7 +6,7 @@
   {
       $name = $args["name"] ?? "stranger";
       $greeting = testIncludeFn("Hello $name!") ;
-      $greeting = testingFunction2();
+      testingFunction2();
       // echo $greeting;
       
       // $greeting = "Hello $name!";

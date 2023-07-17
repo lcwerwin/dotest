e42372968b806d2f7a7a0d6c92e8819d96082457
Erwin <lcwerwin@gmail.com>
2023-07-17 19:07:34 +0800

    Update hello.php

--- dotest/packages/sample/hello/hello.php
+++ dotest/packages/sample/hello/hello.php
@@ -1,17 +1,17 @@
 <?php
-
-  
+ 
 
   function main(array $args) : array
   {
-      $name = $args["name"] ?? "stranger"
+      $name = $args["name"] ?? "stranger";
       $greeting = testIncludeFn("Hello $name!") ;
       
-      $greeting2 = testingFnFromCommon();
+      // $greeting2 = testingFnFromCommon();
       error_log("Start");
+      echo "test message";
       
       // $greeting = "Hello $name!";
-      return ["body" => $greeting  . $greeting2];
+      return ["body" => $greeting  ];
   }
 
   function testIncludeFn($param)

2e5848876942f32400437d7c24d25f3ec18ac8f0
Erwin <lcwerwin@gmail.com>
2023-07-17 19:19:41 +0800

    Update hello.php

--- dotest/packages/sample/hello/hello.php
+++ dotest/packages/sample/hello/hello.php
@@ -1,18 +1,18 @@
 <?php
  
-  include_once "include.php";
+  // include_once "include.php";
 
   function main(array $args) : array
   {
       $name = $args["name"] ?? "stranger";
       $greeting = testIncludeFn("Hello $name!") ;
       
-      $greeting2 = testSameFolder($greeting);
+      // $greeting2 = testSameFolder($greeting);
       error_log("Start");
       echo "test message";
       
       // $greeting = "Hello $name!";
-      return ["body" => $greeting  . $greeting2];
+      return ["body" => $greeting ];
   }
 
   function testIncludeFn($param)
